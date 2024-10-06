1eb45ed97969acae3b53fb56bfb50956b1721d39
Tran The Bao <thebaoa1162003@gmail.com>
2024-10-06 09:55:57 +0700

    addfile

---
+++ frontend/TodayTask.js
@@ -0,0 +1,9 @@
+// Function to navigate back to the calendar
+function goToCalendar() {
+    window.location.href = "Calendar.html"; // Navigate to the main calendar view
+}
+
+// Function to go to the Add Task page
+function goToAddTask() {
+    window.location.href = "Addtask.html"; // Navigate to the task list view
+}
